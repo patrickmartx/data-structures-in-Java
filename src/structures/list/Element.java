656cde1fd3501bcdbcd4af568c0ce38aed0365d1
Patrick Martins <patrick.martins@dataprev.gov.br>
2023-11-06 12:37:46 -0300

    Construíndo uma lista ligada.

---
+++ src/structures/list/Element.java
@@ -0,0 +1,26 @@
+package structures.list;
+
+public class Element {
+    private String value;
+    private Element next;
+
+    public Element(String newValue) {
+        this.value = newValue;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Element getNext() {
+        return next;
+    }
+
+    public void setNext(Element next) {
+        this.next = next;
+    }
+}
