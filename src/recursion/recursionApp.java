6d843e1053b5b709185d74821e35f76c65609329
Patrick Martins <patrick.martins@dataprev.gov.br>
2023-11-08 11:02:51 -0300

    feat: created recursion example.

---
+++ src/recursion/recursionApp.java
@@ -0,0 +1,28 @@
+package recursion;
+
+import utils.PrintFormatting;
+
+public class recursionApp {
+    public static void main(String[] args) {
+        PrintFormatting pf = new PrintFormatting();
+        int[] vector = new int[10];
+
+        for (int i=0; i < vector.length; i++) {
+            vector[i] = (int) (Math.random() * vector.length);
+        }
+
+        pf.printInVector(vector);
+
+        System.out.println();
+
+        System.out.println(sum(0, 0, vector));
+    }
+
+    private static int sum(int sumResult, int position, int[] vector) {
+        if (position < vector.length) {
+            sumResult += vector[position];
+            return sum(sumResult, position + 1, vector);
+        }
+        return sumResult;
+    }
+}
