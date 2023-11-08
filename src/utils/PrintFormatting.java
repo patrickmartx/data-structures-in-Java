9d75b4c305463a6b4f1031b02136102c8df0e9fc
Patrick Martins <patrick.martins@dataprev.gov.br>
2023-11-08 11:28:58 -0300

    refactor: change in the way printing lists.

--- src/utils/PrintFormatting.java
+++ src/utils/PrintFormatting.java
@@ -18,15 +18,25 @@ public class PrintFormatting {
     }
 
     public void printInVector(String[] values) {
+        System.out.print("[ ");
         for (int i = 0; i < values.length; i++){
-            System.out.println(values[i]);
+            System.out.print(values[i]);
+            if (i != values.length - 1) {
+                System.out.print(", ");
+            }
         }
+        System.out.print(" ]\n");
     }
 
     public void printInVector(int[] values) {
+        System.out.print("[ ");
         for (int i = 0; i < values.length; i++){
-            System.out.println(values[i]);
+            System.out.print(values[i]);
+            if (i != values.length-1) {
+                System.out.print(", ");
+            }
         }
+        System.out.print(" ]\n");
     }
 
 }
